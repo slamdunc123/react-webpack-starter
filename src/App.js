bcd8954e19cf6558218abff06e04ced210afadc1
Duncan Laidlaw <slamdunc123@gmail.com>
2020-03-03 21:43:17 +0000

    intial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './components/layout/Main';
+
+const App = () => {
+    return (
+				<div>
+						<h1>My React App</h1>
+						<Main />
+				</div>
+		)
+}
+
+export default App;
---
+++ src/components/partials/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Button = () => {
+	return (
+		<div>
+			<button onClick={() => console.log('My Button Clicked')}>
+				My Button
+			</button>
+		</div>
+	);
+};
+
+export default Button;
